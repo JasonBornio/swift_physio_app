a4a3b6ef3f51ba41a9016a5a22ea85b3c4cf3e20
Justin Ugwudike <jknu2001@gmail.com>
2024-08-27 07:56:45 +0100

    Initial Commit

---
+++ swift_physio_app/swift_physio_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_physio_appApp.swift
+//  swift_physio_app
+//
+//  Created by Justin U on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_physio_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
